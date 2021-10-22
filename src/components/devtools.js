b13d67ed4b0873c9e975b1b47924ff7f2004fc7c
Amitkanswal <amit.kanswal@contentstack.com>
2021-10-22 20:04:47 +0530

    fix:remove copy option and updated version [ECO-188]

--- src/components/devtools.js
+++ src/components/devtools.js
@@ -1,33 +1,44 @@
-import React from 'react';
-import ReactJson from 'react-json-view';
+import React from "react";
+import ReactJson from "react-json-view";
 const DevTools = ({ response }) => {
   return (
     <div
-      className='modal fade'
-      id='staticBackdrop'
-      data-bs-backdrop='static'
-      data-bs-keyboard='false'
-      tabIndex='-1'
-      aria-labelledby='staticBackdropLabel'
-      aria-hidden='true'
+      className="modal fade"
+      id="staticBackdrop"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabIndex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
     >
-      <div className='modal-dialog modal-lg'>
-        <div className='modal-content'>
-          <div className='modal-header'>
-            <h2 className='modal-title' id='staticBackdropLabel'>
-            Json Response
+      <div className="modal-dialog modal-lg">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h2 className="modal-title" id="staticBackdropLabel">
+              Json Response
             </h2>
             <button
-              type='button'
-              className='btn-close'
-              data-bs-dismiss='modal'
-              aria-label='Close'
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
             ></button>
           </div>
-          <div className='modal-body'>
-            {response ? <pre id='jsonViewer'>
-              {response && <ReactJson src={response} collapsed/>}
-            </pre> : ''}
+          <div className="modal-body">
+            {response ? (
+              <pre id="jsonViewer">
+                {response && (
+                  <ReactJson
+                    src={response}
+                    collapsed
+                    name="response"
+                    enableClipboard={false}
+                  />
+                )}
+              </pre>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </div>
